936bfd841d47ed1425d535b24bd813cf74b8e3f1
KenMeagher <ungadin@yahoo.com>
2018-03-24 12:04:21 -0500

    added 2 files

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello!")
+const express = require('express')
+const app = express()
+var server = app.listen(3000, function() { console.log(new Date().toISOString() + ': server listening on port 3000');
+});
+app.use(express.static('public'))
